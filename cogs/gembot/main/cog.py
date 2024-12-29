955b1b666f396958c7bc057e56fc7a4919ac030b
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-12-28 19:16:47 -0600

    added basic gem system

--- cogs/gembot/main/cog.py
+++ cogs/gembot/main/cog.py
@@ -4,16 +4,31 @@ import json
 
 
 class GemBot(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: discord.Bot):
         self.bot = bot
-        self.jsonLoad = json.load(open('./config.json', 'r', encoding='utf-8'))['GemBot']  # https://stackoverflow.com/questions/65063828/emoji-problem-reading-json-file-in-python
+        self.jsonLoad = json.load(open('./config.json', 'r', encoding='utf-8'))[
+            'GemBot']  # https://stackoverflow.com/questions/65063828/emoji-problem-reading-json-file-in-python
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload):
         emoji = payload.data['emoji']['name']
-        print('why', emoji, self.jsonLoad['GemEmoji'])
         if emoji == self.jsonLoad['GemEmoji']:
-            print(payload)
+            gemChannel = await self.bot.fetch_channel(self.jsonLoad['channelID'])
+            m = await (await self.bot.fetch_channel(payload.channel_id)).fetch_message(payload.message_id)  # message
+            gemScore = discord.utils.get(m.reactions, emoji=self.jsonLoad['GemEmoji']).count
+
+            if gemScore == int(self.jsonLoad['minCount']):
+                embed = discord.Embed(
+                    title=f"Message by: {m.author.name}",
+                    description=m.content,
+                    color=discord.Color.teal()
+                )
+                embed.add_field(name=f'Message Link', value=f'https://discord.com/channels/{m.guild.id}/{m.channel.id}/{m.id}', inline=False)
+                embed.add_field(name=f'Gem Score', value=f"{gemScore}")
+                await gemChannel.send(embed=embed)
+            elif gemScore >= int(
+                    self.jsonLoad['minCount']):
+                pass
 
 
 def setup(bot: discord.Bot):
